d77d333e26f5ebfbb2258f8bed9cf41496f36470
kawabata2018 <chuanduanku52@gmail.com>
2025-08-20 01:21:59 +0900

    feat: goライブラリバージョンアップ line-bot-sdk-go/v7 => v8

--- cloudrun/handler.go
+++ cloudrun/handler.go
@@ -6,7 +6,7 @@ import (
 	"log/slog"
 
 	"github.com/caarlos0/env/v11"
-	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/line/line-bot-sdk-go/v8/linebot"
 )
 
 type linebotConfig struct {
